963406742abc97a98fc728e962dff62213990d57
Mialy <mialiharisoaranaivo@gmail.com>
2023-06-28 15:44:34 +0300

    first page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+  
+  root to: 'pages#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/decode', to: 'pages#decode'
 end
+ 
