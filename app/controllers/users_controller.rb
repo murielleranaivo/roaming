742a8859374d884a4373d572f5f494f91cc161f2
Mialy <mialiharisoaranaivo@gmail.com>
2023-07-07 13:18:28 +0300

    implement register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
 
-    def signup
-
+    def register
+        @user = User.new
     end
 
-    def login
-
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to '/login'
+        else
+            render :register
+        end
     end
 
+    private
+
+        def user_params
+            params.require(:user).permit(:username, :email, :password_digest, :password_digest)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do 
+  
+  root to: 'sessions#login'
+  get '/login', to: 'sessions#login'
 
-  get '/login', to: 'users#login'
+  post '/sessions', to: 'sessions#create'
+
+  get '/sign_out', to: 'sessions#destroy'
+
+  get '/register', to: 'users#register'
+
+  post '/users', to: 'users#create'
+  
+  get '/index', to: 'pages#index', as: 'index'
+
 
 end
  
